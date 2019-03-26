f0c2f964dbdb1375c144e1377073bd6e63c1028e
nazarmoshniaha <nazarofukraine1@gmail.com>
2019-03-26 06:24:06 +0200

    first commit

--- js/list.js
+++ js/list.js
@@ -4,10 +4,21 @@ $(function(){
     
         $('.technology_pctr').css({'grid-column':'1/3'});
         $('.technology_text').css({'grid-column':'3/5'});
+        $('.technology_pctr').css({'grid-row':'3/4'});
+        $('.technology_text').css({'grid-row':'3/4'});
+
         $('.advantages').css({'padding-left':'25%'});
         $('.advantages').css({'padding-right':'25%'});
         $('.advantages').css({'grid-template-columns':'repeat(2, 1fr)'});
         $('.advantages').css({'grid-template-rows':'1fr 1fr'});
+        $('.advantages').css({'grid-row':'4/5'});
+        $('.advantages').css({'grid-row':'4/5'});
+
+        $('.sale_pctr').css({'grid-column':'1/3'});
+        $('.sale_form').css({'grid-column':'3/5'});
+
+        $('.slogan_pctr').css({'grid-row':'2/3'});
+        $('.slogan_text').css({'grid-row':'2/3'});
       });
     
 
@@ -15,13 +26,41 @@ $(function(){
     
         $('.technology_pctr').css({'grid-column':'3/5'});
         $('.technology_text').css({'grid-column':'1/3'});
+        $('.technology_pctr').css({'grid-row':'2/3'});
+        $('.technology_text').css({'grid-row':'2/3'});
+
+        $('.slogan_pctr').css({'grid-row':'4/5'});
+        $('.slogan_text').css({'grid-row':'4/5'});
+
+        $('.advantages').css({'grid-row':'3/4'});
+        $('.advantages').css({'padding':'0'});
+        $('.advantages').css({'grid-template-columns':'repeat(4, 1fr)'});
+        $('.advantages').css({'grid-template-rows':'auto'});
+        //$('.advantages').css({'padding-left':'25%'});
+        //$('.advantages').css({'padding-right':'25%'});
+        //$('.advantages').css({'grid-template-columns':'repeat(2, 1fr)'});
     });
+
 
     $('.but3').on('click', function(){
     
         $('.advantages').css({'padding':'0'});
         $('.advantages').css({'grid-template-columns':'repeat(4, 1fr)'});
         $('.advantages').css({'grid-template-rows':'auto'});
+
+        $('.sale_pctr').css({'grid-column':'3/5'});
+        $('.sale_form').css({'grid-column':'1/3'});
+        $('.sale_pctr').css({'grid-row':'5/6'});
+        $('.sale_form').css({'grid-row':'5/6'});
+
+        $('.technology_pctr').css({'grid-column':'1/3'});
+        $('.technology_text').css({'grid-column':'3/5'});
+        $('.technology_pctr').css({'grid-row':'4/5'});
+        $('.technology_text').css({'grid-row':'4/5'});
+
+        $('.slogan_pctr').css({'grid-row':'2/3'});
+        $('.slogan_text').css({'grid-row':'2/3'});
+
     });
  
 });
